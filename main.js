fc683e28c5e867a2f7f973e09ec96c54ad9b29c2
AbidemiT <abidemis01@gmail.com>
2019-10-01 20:55:09 +0100

    Added Sidebar Functionality

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
 // Menu functionality
 
-(function () {
+(() => {
     let menu = document.querySelector("ul");  
-    let burger = document.querySelector(".burger");
+    let burger = document.querySelector(".menu");
+    let anc = document.querySelectorAll(".link");
+    let link = [...anc]    
     
+    let li = document.createElement('li');
+
     burger.addEventListener("click", (e) => {
-        menu.classList.toggle("menu");
+        li.innerHTML = "X";
+        menu.prepend(li);
+        menu.style.display = "block";
+        menu.classList.toggle("sidebar");
         e.preventDefault();
+    })
+
+    li.addEventListener("click", (e) => {
+        menu.style.display = "none";
+        menu.classList.toggle("sidebar");
+    })
+
+    link.forEach(a => {
+        a.addEventListener('click', (e) => {
+            menu.style.display = "none";
+            menu.classList.remove("sidebar");
+        })
     })
 })();
